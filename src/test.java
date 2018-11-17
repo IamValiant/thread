5b5dfeae337314f8a813824b27269c392b04e6b8
xueyongzeng <xueyongzeng@yangqianguan.com>
2018-11-17 15:52:41 +0800

    six

--- src/test.java
+++ src/test.java
@@ -10,6 +10,7 @@ public class test {
 		System.out.println("test for three!");
 		System.out.println("test for four!");
 		System.out.println("test for five!");
+		System.out.println("test for six change!");
 	}
 
 }
